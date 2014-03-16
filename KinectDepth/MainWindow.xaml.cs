f8fd0d503311afdb57cf2c0b5f9cb46bd858ba7e
U-Ryota-PC\Ryota <Ryota@Ryota-PC.(none)>
2014-03-16 17:10:35 +0900

    copy base from KinectBase

--- KinectDepth/MainWindow.xaml.cs
+++ KinectDepth/MainWindow.xaml.cs
@@ -22,7 +22,66 @@ namespace KinectDepth
     {
         public MainWindow()
         {
-            InitializeComponent();
+            try
+            {
+                InitializeComponent();
+
+                // Kinectの接続確認
+                if (KinectSensor.KinectSensors.Count == 0)
+                {
+                    throw new Exception("Kinectが接続されていません");
+                }
+
+                // Kinectの動作を開始
+                StartKinect(KinectSensor.KinectSensors[0]);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+        }
+
+        /**
+         * Kinectの動作を開始する 
+         * 
+         * @param KinectSensor.KinectSensors[0]
+         * 
+         */
+        private void StartKinect(KinectSensor kinect)
+        {
+            // Kinectの動作開始
+            kinect.Start();
+        }
+
+        /**
+         * Kinectの動作を停止する 
+         * 
+         * @param KinectSensor.KinectSensors[0]
+         * 
+         */
+        private void StoptKinect(KinectSensor kinect)
+        {
+            if (kinect == null)
+            {
+                if (kinect.IsRunning)
+                {
+                    // Kinectの動作を停止
+                    kinect.Stop();
+                    // ネイティブソースの解放
+                    kinect.Dispose();
+                }
+            }
+        }
+
+
+        /*
+         * 終了時のイベント
+         * 
+         */
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs ce)
+        {
+            StoptKinect(KinectSensor.KinectSensors[0]);
         }
     }
 }
